9fb5e0eba3f29cd93ce1a8e549d64111940350ec
buckminster-fuller <158994125+buckminster-fuller@users.noreply.github.com>
2025-03-07 10:43:03 +0100

    preprocessor for ssd1306wire.h usage
    d-duino dev board display support

--- minigotchi/display.h
+++ minigotchi/display.h
@@ -1,44 +1,30 @@
 /**
- * display.h: header files for display.cpp
+ * display.h: header file for display.cpp
  */
 
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
 #include "config.h"
-#include <Adafruit_GFX.h>
-#include <Adafruit_SH110X.h>
-#include <Adafruit_SSD1305.h>
-#include <Adafruit_SSD1306.h>
-#include <SPI.h>
-#include <U8g2lib.h>
-#include <Wire.h>
+
+#ifdef USE_SSD1306WIRE
+  // For d-duino board only
+  #include "SSD1306Wire.h"
+#else
+  // For other boards (not used in this configuration)
+  #include <Adafruit_GFX.h>
+  #include <Adafruit_SH110X.h>
+  #include <Adafruit_SSD1305.h>
+  #include <Adafruit_SSD1306.h>
+  #include <SPI.h>
+  #include <U8g2lib.h>
+  #include <Wire.h>
+#endif
 
 #define SSD1306_SCREEN_WIDTH 128
 #define SSD1306_SCREEN_HEIGHT 64
 
-#define SSD1305_SCREEN_WIDTH 128
-#define SSD1305_SCREEN_HEIGHT 32
-
-#define SSD1305_OLED_CLK 14
-#define SSD1305_OLED_MOSI 13
-#define SSD1305_OLED_CS 15
-#define SSD1305_OLED_DC 4
-#define SSD1305_OLED_RESET 5
-
-#define IDEASPARK_SSD1306_SCL 14
-#define IDEASPARK_SSD1306_SDA 12
-
-#define SH1106_SCREEN_WIDTH 128
-#define SH1106_SCREEN_HEIGHT 64
-#define SH1106_OLED_RESET -1
-
-#define SH1106_SCL 5
-#define SH1106_SDA 4
-
-// Init screen info
-#define SSD1306_OLED_RESET -1
-#define WEMOS_OLED_SHIELD_OLED_RESET 0 // GPIO0
+// (Other definitions could remain if needed)
 
 class Display {
 public:
@@ -47,8 +33,10 @@ public:
   static void updateDisplay(String face);
   static void updateDisplay(String face, String text);
 
-private:
+#ifndef USE_SSD1306WIRE
+  // Only needed if using the U8G2 branch for text-wrapping
   static void printU8G2Data(int x, int y, const char *data);
+#endif
 };
 
 #endif // DISPLAY_H
